8205478001582e092f62a8b3b1f7e7236ad3658e
Aravindh <aravindhbme@gmail.com>
2021-04-27 16:45:15 +0530

    first commit

--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp10
+public class stock
 {
-    class Program
+    public int stocks;
+    public int inventory_AS;
+    public int addstock;
+}
+public class Product : stock
+{
+    public string p_name;
+    public double price;
+    public int quantity;
+    public double total;
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Product soap = new Product();
+        soap.total = soap.price * soap.quantity;
+        Console.WriteLine("enter the product name");
+        soap.p_name = Console.ReadLine();
+        Console.WriteLine("enter the quantity");
+        soap.quantity = Convert.ToInt32(Console.ReadLine());
+        soap.total = (soap.price) * (soap.quantity);
+        Console.WriteLine("enter the price");
+        soap.price = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("enter the stocks available");
+        soap.stocks = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter the amount of newly added stocks");
+        soap.addstock = Convert.ToInt32(Console.ReadLine());
+        soap.stocks = soap.stocks + soap.addstock;
+        soap.inventory_AS = soap.stocks - soap.quantity;
+        soap.total = soap.quantity * soap.price;
+
+
+
+        Console.WriteLine("s.no            : " + "1");
+        Console.WriteLine("product         : " + soap.p_name + "   ");
+        Console.WriteLine("price           : " + soap.price + "  ");
+        Console.WriteLine("quantity        : " + soap.quantity + " ");
+        Console.WriteLine("total price     : " + soap.total + "   ");
+        Console.WriteLine("remaining stocks: " + soap.inventory_AS);
+        Console.ReadLine();
     }
+
 }
