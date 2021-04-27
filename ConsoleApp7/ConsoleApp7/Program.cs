12eda7e3d4682cd72d312d7ab7dd9bf121d0ec90
Aravindh <aravindhbme@gmail.com>
2021-04-27 15:16:24 +0530

    first commit

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Class4
+    {
+
+        public void Main()
+        {
+            Class4 obj = new Class4();
+            int exitkey = 1;
+            while (exitkey > 0)
+            {
+                Console.WriteLine("You want to exit ? Y/N");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "n")
+                {
+                    obj.InputX();
+                    choice = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y;
+            try
+            {
+                y = x / 0;
+                Console.WriteLine(y);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
